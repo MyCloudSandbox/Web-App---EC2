018c3757c7db3c4a17236bae463b729cd5cbff18
mnewt55 <144187288+mnewt55@users.noreply.github.com>
2023-12-05 22:47:37 -0500

    Create 15-IAM-Memberships.tf

---
+++ 15-IAM-Memberships.tf
@@ -0,0 +1,32 @@
+#This is to associate a user with a group
+resource "aws_iam_user_group_membership" "Directors" {
+  user = aws_iam_user.Director1.name
+
+  groups = [
+    aws_iam_group.Directors.name,
+  ]
+}
+
+resource "aws_iam_user_group_membership" "Managers" {
+  user = aws_iam_user.Manager1.name
+
+  groups = [
+    aws_iam_group.Managers.name,
+  ]
+}
+
+resource "aws_iam_user_group_membership" "Associates" {
+  user = aws_iam_user.Associate1.name
+
+  groups = [
+    aws_iam_group.Associates.name,
+  ]
+}
+
+resource "aws_iam_user_group_membership" "Trainees" {
+  user = aws_iam_user.Trainee1.name
+
+  groups = [
+    aws_iam_group.Trainees.name,
+  ]
+}
