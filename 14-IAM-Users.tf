6b4d5161d4940098df404cefdd1d8178ff124b21
mnewt55 <144187288+mnewt55@users.noreply.github.com>
2023-12-05 22:46:23 -0500

    Create 14-IAM-Users.tf

---
+++ 14-IAM-Users.tf
@@ -0,0 +1,16 @@
+#This is for creating a user that will go into a group
+resource "aws_iam_user" "Director1" {
+    name = "Director1"
+}
+
+resource "aws_iam_user" "Manager1" {
+    name = "Manager1"
+}
+
+resource "aws_iam_user" "Associate1" {
+    name = "Associate1"
+}
+
+resource "aws_iam_user" "Trainee1" {
+    name = "Trainee1"
+}
